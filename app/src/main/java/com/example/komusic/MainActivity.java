dce804cce375f68ac4a891918b180fb72cb322dd
Tran Cong Dung <dung.tran21@pn.com.vn>
2021-01-26 17:23:19 +0700

    add homepage

--- app/src/main/java/com/example/komusic/MainActivity.java
+++ app/src/main/java/com/example/komusic/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.komusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rcvPlaylist;
+    private RecyclerView rcvRecentSong;
+    ArrayList<Playlist> arrayList;
+    ArrayList<Song> arrayListSong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homepage);
+        rcvPlaylist = findViewById(R.id.rycPlaylist);
+        int banner[] = {R.drawable.banner1,R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5};
+        arrayList = new ArrayList<>();
+
+        rcvPlaylist.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        rcvPlaylist.setItemAnimator(new DefaultItemAnimator());
+
+        for (int i = 0; i < banner.length; i++) {
+            Playlist itemModel = new Playlist();
+            itemModel.setResourceId(banner[i]);
+
+            //add in array list
+            arrayList.add(itemModel);
+        }
+
+        PlaylistAdapter adapter = new PlaylistAdapter(getApplicationContext(), arrayList);
+        rcvPlaylist.setAdapter(adapter);
+
+
+        //recent song
+
+        rcvRecentSong = findViewById(R.id.rycRecentSong);
+        rcvRecentSong.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        rcvRecentSong.setItemAnimator(new DefaultItemAnimator());
+        SongAdapter adapterSong = new SongAdapter(getApplicationContext(), getListSong());
+        rcvRecentSong.setAdapter(adapterSong);
+
+        //made for you
+        rcvRecentSong = findViewById(R.id.rycMadeForYou);
+        rcvRecentSong.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        rcvRecentSong.setItemAnimator(new DefaultItemAnimator());
+        SongAdapter adapterForYou = new SongAdapter(getApplicationContext(), getListSong());
+        rcvRecentSong.setAdapter(adapterForYou);
+
+        //singer
+        rcvRecentSong = findViewById(R.id.rycSinger);
+        rcvRecentSong.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false));
+        rcvRecentSong.setItemAnimator(new DefaultItemAnimator());
+        SongAdapter adapterSing = new SongAdapter(getApplicationContext(), getListSong());
+        rcvRecentSong.setAdapter(adapterSing);
+    }
+    private List<Song> getListSong() {
+
+        List<Song> list= new ArrayList<>();
+
+        list.add(new Song(R.drawable.song,"Ngày hạnh phúc"));
+        list.add(new Song(R.drawable.song,"Ngày hạnh phúc"));
+        list.add(new Song(R.drawable.song,"Ngày hạnh phúc"));
+        list.add(new Song(R.drawable.song,"Ngày hạnh phúc"));
+        list.add(new Song(R.drawable.song,"Ngày hạnh phúc"));
+
+        return list;
     }
 }
---
+++ app/src/main/java/com/example/komusic/Song.java
@@ -0,0 +1,32 @@
+package com.example.komusic;
+
+import java.util.List;
+
+public class Song {
+    private int resourceId;
+    private String title;
+
+    public Song(int resourceId, String title) {
+        this.resourceId = resourceId;
+        this.title = title;
+    }
+
+    public Song() {
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
